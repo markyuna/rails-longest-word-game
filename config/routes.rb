18181a47ef50e54ffaceadc57a6ee7c995e3f46e
Marcos Suarez <marcossuarezr88@gmail.com>
2022-12-16 00:31:25 +0100

    Longest word game

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  root to: 'games#index'
   get 'new', to: 'games#new', as: :new
   post 'score', to: 'games#score', as: :score
 end
